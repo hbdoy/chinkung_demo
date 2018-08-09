babfb3be73f152f3d607579d8ac0c719a4e5a89b
hbdoy <felicity860128@gmail.com>
2018-08-09 13:31:36 +0800

    add js in base-data 03,04 with firebase

---
+++ base-data/js/03.js
@@ -0,0 +1,71 @@
+var db = firebase.database();
+
+var createBtn = document.querySelector("#createBtn");
+
+createBtn.addEventListener('click', createClient);
+
+function createClient(e) {
+    e.preventDefault();
+    var validate = validateData();
+    if (validate) {
+        if (confirm("確定要建立?")) {
+            db.ref("/client/" + document.querySelector("#client-id").value).set({
+                id: document.querySelector("#client-id").value,
+                name: document.querySelector("#client-name").value,
+                nickName: document.querySelector("#client-nickName").value,
+                uniformNum: document.querySelector("#client-uniformNum").value,
+                principal: document.querySelector("#client-principal").value,
+                principalBirth: document.querySelector("#client-principalBirth").value,
+                contactMan: document.querySelector("#client-contactMan").value,
+                salesMan: document.querySelector("#client-salesMan").value,
+                phone: document.querySelector("#client-phone").value,
+                companyPhone: document.querySelector("#client-companyPhone").value,
+                fax: document.querySelector("#client-fax").value,
+                cellPhone: document.querySelector("#client-cellPhone").value,
+                postalCode: document.querySelector("#client-postalCode").value,
+                email: document.querySelector("#client-email").value,
+                address: document.querySelector("#client-address").value,
+                deliveryAddr: document.querySelector("#client-deliveryAddr").value,
+                invoiceAddr: document.querySelector("#client-invoiceAddr").value,
+                invoiceTitle: document.querySelector("#client-invoiceTitle").value,
+                credits: document.querySelector("#client-credits").value,
+                checkoutDay: document.querySelector("#client-checkoutDay").value,
+                billingDay: document.querySelector("#client-billingDay").value,
+                requestDay: document.querySelector("#client-requestDay").value,
+                note: document.querySelector("#client-note").value,
+            }).then(function () {
+                // console.log("OKOK");
+                alert("建立成功");
+                setTimeout(function () {
+                    location.reload();
+                }, 500);
+            }).catch(function () {
+                // console.log("some error");
+                alert("伺服器發生錯誤，請稍後再試");
+            });
+        }
+    }
+}
+
+function validateData() {
+    var id = document.querySelector("#client-id").value;
+    var name = document.querySelector("#client-name").value;
+    var uniformNum = document.querySelector("#client-uniformNum").value;
+    var postalCode = document.querySelector("#client-postalCode").value;
+    var credits = document.querySelector("#client-credits").value;
+    var checkoutDay = document.querySelector("#client-checkoutDay").value;
+    var billingDay = document.querySelector("#client-billingDay").value;
+    var requestDay = document.querySelector("#client-requestDay").value;
+    var notEmpty = (id != "" && name != "");
+    var isNum = !(isNaN(uniformNum) || isNaN(postalCode) || isNaN(credits) || isNaN(checkoutDay) || isNaN(billingDay) || isNaN(requestDay));
+    var validate = true;
+    if (!notEmpty) {
+        validate = false;
+        alert("編號或名稱不得為空!");
+    }
+    if (!isNum) {
+        validate = false;
+        alert("數字欄位僅允許填入數字");
+    }
+    return validate;
+}
